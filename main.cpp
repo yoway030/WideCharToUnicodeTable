d1586c54d3dea9c1e2dd54b40667d94b7d214ae4
yoway030 <yoway030@gmail.com>
2022-02-10 22:40:04 +0900

    indent

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ constexpr wchar_t HexintToHexwchar(int8_t num)
 {
 	constexpr std::array<wchar_t, 16> HEX_TABLE = {
 		L'0', L'1', L'2', L'3', L'4', L'5', L'6', L'7',
-		L'8', L'9', L'A', L'B', L'C', L'D', L'E', L'F' };
+		L'8', L'9', L'A', L'B', L'C', L'D', L'E', L'F' 
+	};
 	return HEX_TABLE.at(num);
 }
 
@@ -18,10 +19,11 @@ constexpr std::array<wchar_t, 6> WideCharToUnicodeTable(wchar_t ch)
 
 	return std::array<wchar_t, 6>{
 		L'U', L'+',
-			HexintToHexwchar((high & 0xF0) >> 4),
-			HexintToHexwchar(high & 0x0F),
-			HexintToHexwchar((low & 0xF0) >> 4),
-			HexintToHexwchar(low & 0x0F)};
+		HexintToHexwchar((high & 0xF0) >> 4),
+		HexintToHexwchar(high & 0x0F),
+		HexintToHexwchar((low & 0xF0) >> 4),
+		HexintToHexwchar(low & 0x0F)
+	};
 }
 
 int main()
